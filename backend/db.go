7f50fc9b0ca80e5c8908ea981c8756ac154f7f41
noahGms <noah@keshin.net>
2023-05-10 23:34:01 +0200

    setup database with gorm in backend

---
+++ backend/db.go
@@ -0,0 +1,21 @@
+package backend
+
+import (
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func InitDb() (*gorm.DB, error) {
+	db, err := gorm.Open(sqlite.Open("gorm.db"), &gorm.Config{})
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.AutoMigrate()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
--- backend/init.go
+++ backend/init.go
@@ -1,13 +1,26 @@
 package backend
 
-import "context"
+import (
+	"context"
+	"fmt"
+
+	"gorm.io/gorm"
+)
 
 type Backend struct {
 	ctx *context.Context
+	db  *gorm.DB
 }
 
 func NewBackend(ctx *context.Context) *Backend {
+	db, err := InitDb()
+
+	if err != nil {
+		fmt.Printf("%s", err)
+	}
+
 	return &Backend{
 		ctx: ctx,
+		db:  db,
 	}
 }
